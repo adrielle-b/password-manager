e1d42d07ef887282650cacd8a9f5683b961819bb
adrielle-b <adriielle@gmail.com>
2024-01-08 12:52:43 -0300

    feat03: renderizar condicionalmente

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,31 +1,40 @@
+import React, { useState } from 'react';
+
 function Form() {
+  const [showForm, setShowForm] = useState(false);
   return (
-    <form>
+    showForm ? (
+      <form>
 
-      <label htmlFor="name">
-        Nome do serviço
-        <input type="text" id="name" />
-      </label>
+        <label htmlFor="name">
+          Nome do serviço
+          <input type="text" id="name" />
+        </label>
 
-      <label htmlFor="login">
-        Login
-        <input type="text" id="login" />
-      </label>
+        <label htmlFor="login">
+          Login
+          <input type="text" id="login" />
+        </label>
 
-      <label htmlFor="senha">
-        Senha
-        <input type="password" id="senha" />
-      </label>
+        <label htmlFor="senha">
+          Senha
+          <input type="password" id="senha" />
+        </label>
 
-      <label htmlFor="url">
-        URL
-        <input type="text" id="url" />
-      </label>
+        <label htmlFor="url">
+          URL
+          <input type="text" id="url" />
+        </label>
 
-      <button type="button">Cadastrar</button>
-      <button type="button">Cancelar</button>
+        <button type="button">Cadastrar</button>
+        <button type="button" onClick={ () => setShowForm(false) }>Cancelar</button>
 
-    </form>
+      </form>
+    ) : (
+      <button type="button" onClick={ () => setShowForm(true) }>
+        Cadastrar nova senha
+      </button>
+    )
   );
 }
 
