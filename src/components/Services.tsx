d91b94341b598fb27075eb9b01e5a4498499355c
adrielle-b <adriielle@gmail.com>
2024-01-08 16:59:10 -0300

    feat07: apaga serviço cadastrado

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,18 +2,30 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalProvider';
 
 function Services() {
-  const { services } = useContext(GlobalContext);
+  const { services, setServices } = useContext(GlobalContext);
+
+  const removeService = (index: number) => {
+    const newListServices = services.filter((_, i) => i !== index);
+    setServices(newListServices);
+  };
+
   return (
     <section>
       {services.length === 0 ? (<p>Nenhuma senha cadastrada</p>) : (
         <>
-          {services.map((service) => (
+          {services.map((service, index) => (
             <div key={ service.name }>
               <a href={ service.url }>{ service.name }</a>
               <p>Login:</p>
               <p>{ service.login }</p>
               <p>Senha:</p>
               <p>{ service.senha }</p>
+              <button
+                type="button"
+                onClick={ () => removeService(index) }
+              >
+                Remover
+              </button>
             </div>
           ))}
         </>
