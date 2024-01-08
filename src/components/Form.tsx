29fa2a4957e898b9f70c426a34f3ed6328a23ce7
adrielle-b <adriielle@gmail.com>
2024-01-08 13:16:19 -0300

    feat04: validaçao form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,31 +2,74 @@ import React, { useState } from 'react';
 
 function Form() {
   const [showForm, setShowForm] = useState(false);
+  const [inputs, setInputs] = useState({
+    name: '',
+    login: '',
+    senha: '',
+    url: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({
+      ...inputs,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const formIsValid = () => {
+    const { name, login, senha } = inputs;
+
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+    const passwordCheck = passwordRegex.test(senha);
+
+    const isValid = name.length > 0 && login.length > 0 && passwordCheck;
+    return isValid;
+  };
+
   return (
     showForm ? (
       <form>
 
         <label htmlFor="name">
           Nome do serviço
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={ inputs.name }
+            onChange={ (e) => handleChange(e) }
+          />
         </label>
 
         <label htmlFor="login">
           Login
-          <input type="text" id="login" />
+          <input
+            type="text"
+            id="login"
+            value={ inputs.login }
+            onChange={ (e) => handleChange(e) }
+          />
         </label>
 
         <label htmlFor="senha">
           Senha
-          <input type="password" id="senha" />
+          <input
+            type="password"
+            id="senha"
+            value={ inputs.senha }
+            onChange={ (e) => handleChange(e) }
+          />
         </label>
 
         <label htmlFor="url">
           URL
-          <input type="text" id="url" />
+          <input
+            type="text"
+            id="url"
+            onChange={ (e) => handleChange(e) }
+          />
         </label>
 
-        <button type="button">Cadastrar</button>
+        <button type="button" disabled={ !formIsValid() }>Cadastrar</button>
         <button type="button" onClick={ () => setShowForm(false) }>Cancelar</button>
 
       </form>
